1694efb8ffc38851358efd290582c4189168802d
Erlangga Dewa <erlanggadewa7@gmail.com>
2022-01-02 13:44:49 +0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Front End Kiranagara for Bytesfest 2021
+
+How to use
+
+1. Install nodeJS
+2. Run : npm install
+3. Run : npm run
+4. Program running at <http://localhost:3000/>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./views/*.ejs", "./views/*/*.ejs", "./views/*/*/*.ejs"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      inset: { "1/5": "20%" },
+      left: { "4/5": "80%" },
+      right: { "1/5": "20%" },
+      colors: {
+        "main-green": "#d7e2e8",
+        "orange-bar": "#fe5e39",
+        "orange-light": "rgba(255, 196, 0, 0.692)",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      // width: ["hover"],
+      // visibility: ["hover", "focus", "responsive", "group-hover"],
+      // padding: ["hover", "focus", "responsive", "group-hover"],
+      // display: ["hover", "focus", "responsive", "group-hover"],
+      // margin: ["hover", "focus", "responsive", "group-hover"],
+      // transitionDuration: ["hover", "focus", "responsive", "group-hover"],
+    },
+  },
+  plugins: [],
+};
